f47e6fce218818a798c1ee18e0b3fc259fb6848e
uditnarayan13 <un11@iitbbs.ac.in>
2022-01-26 16:31:20 +0530

    Gap method Added

--- Arrays/mergeSortedArrays.cpp
+++ Arrays/mergeSortedArrays.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+int calculateGap(int gap){
+    if(gap<=1){
+        return 0;
+    }else{
+        return ((gap/2)+ (gap%2));
+    }
+}
+void mergeGapmethod(int arr1[],int arr2[],int n, int m){
+    int gap=n+m;
+    for(gap=calculateGap(gap);gap>0;gap=calculateGap(gap)){
+        // swap the elements of first array
+        int i,j,k;
+        for(i=0;i+gap<n;i++){
+            if(arr1[i]>arr1[i+gap]){
+                swap(arr1[i],arr1[i+gap]);
+            }
+        }
+        //swap the elements between two arrays
+        for(j=gap>n?gap-n:0;i<n && j<m ;i++,j++){
+            if(arr1[i]>arr2[j]){
+                swap(arr1[i],arr2[j]);
+            }
+        }
+        //swap the second array elements so that it can be sorted
+        for(j=0;j+gap<m;j++){
+            if(arr2[j]>arr2[j+gap]){
+                swap(arr2[j],arr2[j+gap]);
+            }
+        }
+    }
+    
+}
 void mergeArray(int arr1[], int arr2[], int n, int m)
 {
     for (int i = 0; i < n; i++)
@@ -43,7 +75,8 @@ int main()
     {
         cin >> arr2[i];
     }
-    mergeArray(arr1, arr2, n, m);
+    // mergeArray(arr1, arr2, n, m);
+    mergeGapmethod(arr1,arr2,n,m);
     cout<<"Array 1 : ";
     for (int i = 0; i < n; i++)
     {

9ddefccc3b96dbb31ca245644e73eb0201da6377
uditnarayan13 <un11@iitbbs.ac.in>
2022-06-20 22:13:13 +0530

    Recursive solution to longest common subsequence

---
+++ Dynamic Programming/longestCommonSubsequence(Recursion).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int LCS(string s1, string s2, int n, int m)
+{
+	// base-Condition
+	if (n == 0 || m == 0)
+	{
+		return 0;
+	}
+	// Choice Diagram
+	if (s1[n - 1] == s2[m - 1])
+	{
+		return 1 + LCS(s1, s2, n - 1, m - 1);
+	}
+	else
+	{
+		return max(LCS(s1, s2, n, m - 1), LCS(s1, s2, n - 1, m));
+	}
+}
+
+int main()
+{
+	string s1, s2;
+	cin >> s1 >> s2;
+	int n = s1.length();
+	int m = s2.length();
+	cout << LCS(s1, s2, n, m);
+}
